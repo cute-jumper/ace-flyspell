7b7a13a4513e3496cdb49a678fa0c076cccc87d2
Junpeng Qiu <qjpchmail@gmail.com>
2015-04-01 22:30:27 -0400

    Add README and update commentary in source code

--- ace-flyspell.el
+++ ace-flyspell.el
@@ -23,7 +23,53 @@
 
 ;;; Commentary:
 
-;;
+;; #+TITLE: ace-flyspell
+
+;; Jump to and correct spelling errors using `ace-jump-mode' and flyspell. Inspired
+;; by [[https://github.com/abo-abo/ace-link][abo-abo(Oleh Krehel)]]'s [[https://github.com/abo-abo/ace-link][ace-link]].
+
+;; * Setup
+;;   : (add-to-list 'load-path "/path/to/ace-flyspell.el")
+;;   : (require 'ace-flyspell)
+;;   : (ace-flyspell-setup)
+
+;;   This setup binds the command =ace-flyspell-dwim= to =C-.=, which is originally
+;;   bound to =flyspell-auto-correct-word= if you enable the =flyspell-mode=. Of
+;;   course, you can choose to change the key binding.
+
+;;   Usually, you should enable =flyspell-mode= because this package aims to jump
+;;   to and correct spelling errors detected by =flyspell=, or at least you need to
+;;   run =flyspell-buffer= to detect spelling errors.
+
+;; * Usage
+;;   There are three available commands:
+;; ** =ace-flyspell-jump-word=
+;;    This command jumps to an spelling error using =ace-jump-word-mode=, and move
+;;    the point where the spelling error is.
+;; ** =ace-flyspell-correct-word=
+;;    This command is different from =ace-flyspell-jump-word= in the sense that it
+;;    aims to *correct* rather than *jump to* the spelling error. At first, it
+;;    looks like =ace-flyspell-jump-word=, but after you jump to the misspelt word,
+;;    it will enter another /mode/, where you hit =.= to invoke
+;;    =flyspell-auto-correct-word= to correct the current misspelt word and hit any
+;;    other key to accept the correction and return to the original position.
+
+;;    This command is useful when you're writing an article and want to temporarily
+;;    go back to some spelling error and return to where you left off after fixing
+;;    the error
+;; ** =ace-flyspell-dwim=
+;;    If the word under the cursor is misspelt, then this command is identical to
+;;    =flyspell-auto-correct-word=, otherwise it will call
+;;    =ace-flyspell-correct-word= to jump to and correct some spelling error.
+
+;; * Acknowledgment This package is based on
+;;   [[https://github.com/winterTTr/ace-jump-mode][winterTTr]]'s awesome
+;;   [[https://github.com/winterTTr/ace-jump-mode][ace-jump-mode]] package and
+;;   inspired by [[https://github.com/abo-abo/ace-link][abo-abo(Oleh Krehel)]]'s
+;;   [[https://github.com/abo-abo/ace-link][ace-link]] package, from which I
+;;   borrowed two convenient macros, and this package is kind of like the a
+;;   variant of the original [[https://github.com/abo-abo/ace-link][ace-link]]
+;;   package although I made some further extensions to meet my own needs.
 
 ;;; Code:
 
@@ -46,7 +92,7 @@
 
 (defvar ace-flyspell--original-length 0)
 
-;; Two convinient macros from `ace-link.el', which is a pacakge written by
+;; Two convenient macros from `ace-link.el', which is a pacakge written by
 ;; Oleh Krehel <ohwoeowho@gmail.com>.
 ;; Original code URL: https://github.com/abo-abo/ace-link
 ;; I modified the macro name to conform the naming convention
@@ -171,9 +217,12 @@
           (not (flyspell-word)))
       (flyspell-auto-correct-word)
     (ace-flyspell-correct-word)))
-
-(eval-after-load "flyspell"
-  '(define-key flyspell-mode-map (kbd "C-.") 'ace-flyspell-dwim))
+;;;###autoload
+(defun ace-flyspell-setup ()
+  (interactive)
+  (global-set-key (kbd "C-.") 'ace-flyspell-dwim)
+  (eval-after-load "flyspell"
+    '(define-key flyspell-mode-map (kbd "C-.") 'ace-flyspell-dwim)))
 
 (provide 'ace-flyspell)
 ;;; ace-flyspell.el ends here
